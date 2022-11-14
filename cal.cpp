800b1f40b0787a1e350e2e2077ff04289ec801c6
AkshithaB12 <benikiminnu@gmail.com>
2022-11-14 13:44:40 +0530

    created new file to sub the numbers

--- cal.cpp
+++ cal.cpp
@@ -9,13 +9,17 @@ class cal {
 			y = 0;
 		}
 		int add(int,int);
+		int sub(int,int);
 };
 int cal::add(int x,int y) {
 	return x+y;
 }
+int cal::sub(int x,int y) {
+	return x-y;
+}
 int main(){
 	cal obj;
 	cout<<"add is "<<obj.add(4,6)<<endl;
+	cout<<"sub is "<<obj.sub(6,3)<<endl;
 	return 0;
 }
-#system-programming
